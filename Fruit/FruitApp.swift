004b304ca0ebfec92647d3b617130e4368feb802
keshew <artyom.korotckov2017@yandex.ru>
2025-06-03 00:27:36 +0500

    Initial Commit

---
+++ Fruit/FruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp.swift
+//  Fruit
+//
+//  Created by Артём Коротков on 03.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
